2e5a118c89a7313647f2f7311b6e91ff6c5ab081
Marcela <mvrivera@unitec.edu>
2020-02-05 21:38:17 -0600

    5/2/20 9:38pm

--- src/proyectoprogra/pkg2/Login.java
+++ src/proyectoprogra/pkg2/Login.java
@@ -43,11 +43,27 @@ public class Login extends javax.swing.JFrame {
         jButton5 = new javax.swing.JButton();
         jLabel14 = new javax.swing.JLabel();
         Pagina_principal = new javax.swing.JFrame();
+        jButton7 = new javax.swing.JButton();
         Restablecer = new javax.swing.JFrame();
         jTextField1 = new javax.swing.JTextField();
         jButton6 = new javax.swing.JButton();
         jLabel15 = new javax.swing.JLabel();
         jLabel16 = new javax.swing.JLabel();
+        Perfil_U = new javax.swing.JFrame();
+        N_Perfil = new javax.swing.JLabel();
+        Cu_Perfil = new javax.swing.JLabel();
+        U_Perfil = new javax.swing.JLabel();
+        jLabel17 = new javax.swing.JLabel();
+        jLabel18 = new javax.swing.JLabel();
+        jLabel19 = new javax.swing.JLabel();
+        jLabel20 = new javax.swing.JLabel();
+        jLabel21 = new javax.swing.JLabel();
+        jLabel22 = new javax.swing.JLabel();
+        Co_Perfil = new javax.swing.JLabel();
+        P_Perfil = new javax.swing.JLabel();
+        T_Perfil = new javax.swing.JLabel();
+        jLabel23 = new javax.swing.JLabel();
+        jLabel24 = new javax.swing.JLabel();
         contra_L = new javax.swing.JPasswordField();
         usuario_L = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
@@ -237,15 +253,28 @@ public class Login extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
+        jButton7.setText("Perfil");
+        jButton7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton7ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout Pagina_principalLayout = new javax.swing.GroupLayout(Pagina_principal.getContentPane());
         Pagina_principal.getContentPane().setLayout(Pagina_principalLayout);
         Pagina_principalLayout.setHorizontalGroup(
             Pagina_principalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1185, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, Pagina_principalLayout.createSequentialGroup()
+                .addContainerGap(741, Short.MAX_VALUE)
+                .addComponent(jButton7)
+                .addGap(64, 64, 64))
         );
         Pagina_principalLayout.setVerticalGroup(
             Pagina_principalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 811, Short.MAX_VALUE)
+            .addGroup(Pagina_principalLayout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(jButton7)
+                .addContainerGap(752, Short.MAX_VALUE))
         );
 
         jButton6.setText("Restablecer");
@@ -282,6 +311,97 @@ public class Login extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addComponent(jButton6)
                 .addContainerGap(50, Short.MAX_VALUE))
+        );
+
+        jLabel17.setFont(new java.awt.Font("Dialog", 1, 15)); // NOI18N
+        jLabel17.setText("Nombre: ");
+
+        jLabel18.setFont(new java.awt.Font("Dialog", 1, 15)); // NOI18N
+        jLabel18.setText("Usuario: ");
+
+        jLabel19.setFont(new java.awt.Font("Dialog", 1, 15)); // NOI18N
+        jLabel19.setText("Pais:");
+
+        jLabel20.setFont(new java.awt.Font("Dialog", 1, 15)); // NOI18N
+        jLabel20.setText("Cumpleaños:");
+
+        jLabel21.setFont(new java.awt.Font("Dialog", 1, 15)); // NOI18N
+        jLabel21.setText("Correo:");
+
+        jLabel22.setFont(new java.awt.Font("Dialog", 1, 15)); // NOI18N
+        jLabel22.setText("Tipo de usuario:");
+
+        jLabel23.setFont(new java.awt.Font("Dialog", 1, 15)); // NOI18N
+        jLabel23.setText("Ubicacion:");
+
+        jLabel24.setForeground(new java.awt.Color(255, 0, 0));
+        jLabel24.setText("EDITAR INFORMACION PERSONAL");
+        jLabel24.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel24MouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout Perfil_ULayout = new javax.swing.GroupLayout(Perfil_U.getContentPane());
+        Perfil_U.getContentPane().setLayout(Perfil_ULayout);
+        Perfil_ULayout.setHorizontalGroup(
+            Perfil_ULayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Perfil_ULayout.createSequentialGroup()
+                .addGroup(Perfil_ULayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(Perfil_ULayout.createSequentialGroup()
+                        .addGap(217, 217, 217)
+                        .addGroup(Perfil_ULayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel19)
+                            .addComponent(jLabel18)
+                            .addComponent(jLabel17)
+                            .addComponent(U_Perfil, javax.swing.GroupLayout.PREFERRED_SIZE, 248, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(N_Perfil, javax.swing.GroupLayout.PREFERRED_SIZE, 248, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(P_Perfil, javax.swing.GroupLayout.PREFERRED_SIZE, 248, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(Perfil_ULayout.createSequentialGroup()
+                        .addGap(35, 35, 35)
+                        .addGroup(Perfil_ULayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel20)
+                            .addComponent(jLabel22)
+                            .addComponent(Cu_Perfil, javax.swing.GroupLayout.PREFERRED_SIZE, 333, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(Co_Perfil, javax.swing.GroupLayout.PREFERRED_SIZE, 373, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(T_Perfil, javax.swing.GroupLayout.PREFERRED_SIZE, 373, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel24)
+                            .addComponent(jLabel23)
+                            .addComponent(jLabel21))))
+                .addContainerGap(119, Short.MAX_VALUE))
+        );
+        Perfil_ULayout.setVerticalGroup(
+            Perfil_ULayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Perfil_ULayout.createSequentialGroup()
+                .addGap(65, 65, 65)
+                .addComponent(jLabel17)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(N_Perfil, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(U_Perfil, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel19)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(P_Perfil, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel20)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(Cu_Perfil, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel21)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(Co_Perfil, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel22)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(T_Perfil, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel23)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 99, Short.MAX_VALUE)
+                .addComponent(jLabel24)
+                .addGap(26, 26, 26))
         );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -364,6 +484,7 @@ public class Login extends javax.swing.JFrame {
 
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        usuarios.add(new Usuario("cliente", "marce", "123", "Marcela Eunice Rivera Varela", "Honduras", "mvrivera", "sepa putas", "14/01/20"));
         usuario = usuario_L.getText();
         contra = contra_L.getText();
         for (int i = 0; i < usuarios.size(); i++) {
@@ -373,7 +494,8 @@ public class Login extends javax.swing.JFrame {
                 this.Pagina_principal.setLocationRelativeTo(this);
                 this.setVisible(false);
                 this.Pagina_principal.setVisible(true);
-
+            } else {
+                JOptionPane.showMessageDialog(null, "Usuario no encontrado");
             }
         }
     }//GEN-LAST:event_jButton1ActionPerformed
@@ -411,10 +533,10 @@ public class Login extends javax.swing.JFrame {
                 pa_R.setText("");
                 cor_R.setText("");
                 fe_R.setText("");
-                this.Login.pack();
-                this.Login.setLocationRelativeTo(this);
+
+                this.setLocationRelativeTo(this);
                 this.Registro.setVisible(false);
-                this.Login.setVisible(true);
+                this.setVisible(true);
 
             }
 
@@ -425,13 +547,14 @@ public class Login extends javax.swing.JFrame {
     private void tipo_usuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tipo_usuarioActionPerformed
 
     }//GEN-LAST:event_tipo_usuarioActionPerformed
-
+    int posi = 0;
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         usuario = usuario_L1.getText();
         contra = contra_L1.getText();
         for (int i = 0; i < usuarios.size(); i++) {
             if ((usuarios.get(i).getUsuario()).equals(usuario) && (usuarios.get(i).getContra()).equals(contra)) {
                 System.out.println("bienvenido");
+                posi = i;
                 this.Pagina_principal.pack();
                 this.Pagina_principal.setLocationRelativeTo(this);
                 this.setVisible(false);
@@ -462,6 +585,43 @@ public class Login extends javax.swing.JFrame {
     private void jLabel14MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel14MouseClicked
         // TODO add your handling code here:
     }//GEN-LAST:event_jLabel14MouseClicked
+
+    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
+
+        this.Perfil_U.pack();
+        this.Perfil_U.setLocationRelativeTo(this);
+        this.Pagina_principal.setVisible(false);
+        this.Perfil_U.setVisible(true);
+        N_Perfil.setText(usuarios.get(posi).getNombre());
+        U_Perfil.setText(usuarios.get(posi).getUsuario());
+        P_Perfil.setText(usuarios.get(posi).getPais());
+        Co_Perfil.setText(usuarios.get(posi).getCorreo());
+        Cu_Perfil.setText(usuarios.get(posi).getFecha());
+        T_Perfil.setText(usuarios.get(posi).getTipo());
+
+    }//GEN-LAST:event_jButton7ActionPerformed
+
+    private void jLabel24MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel24MouseClicked
+        int o = Integer.parseInt(JOptionPane.showInputDialog("1.Editar nombre\n2.Editar nickname\n3.Editar Pais\n4.Editar Cumpleaños\n5.Editar Correo\n6.Editar Ubicacion "));
+        switch (o) {
+            case 1:
+                JOptionPane.showInputDialog("Ingrese ");
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+                
+        }
+
+
+    }//GEN-LAST:event_jLabel24MouseClicked
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -496,10 +656,17 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Co_Perfil;
+    private javax.swing.JLabel Cu_Perfil;
     private javax.swing.JFrame Login;
+    private javax.swing.JLabel N_Perfil;
+    private javax.swing.JLabel P_Perfil;
     private javax.swing.JFrame Pagina_principal;
+    private javax.swing.JFrame Perfil_U;
     private javax.swing.JFrame Registro;
     private javax.swing.JFrame Restablecer;
+    private javax.swing.JLabel T_Perfil;
+    private javax.swing.JLabel U_Perfil;
     private javax.swing.JPasswordField co_R;
     private javax.swing.JPasswordField contra_L;
     private javax.swing.JPasswordField contra_L1;
@@ -511,6 +678,7 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
@@ -519,7 +687,15 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel14;
     private javax.swing.JLabel jLabel15;
     private javax.swing.JLabel jLabel16;
+    private javax.swing.JLabel jLabel17;
+    private javax.swing.JLabel jLabel18;
+    private javax.swing.JLabel jLabel19;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel20;
+    private javax.swing.JLabel jLabel21;
+    private javax.swing.JLabel jLabel22;
+    private javax.swing.JLabel jLabel23;
+    private javax.swing.JLabel jLabel24;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
